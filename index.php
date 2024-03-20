e2cc03abf87eee59e93401521275992d47eddfac
prajesh2277 <164074805+prajesh2277@users.noreply.github.com>
2024-03-20 09:33:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <header>
+        <h1>welcome MPKR transport roadway</h1>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="login page.php">Login</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="service.php">Service</a></li>
+                <li><a href="contact.php">contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="truck-container">
+            <img src="download.jpg" alt="error loading image" class="truck">
+        </div> 
+        <section>
+            <h2>Discription.
+            </h2>
+            <p> the intentional movement of humans, animals, and goods from one location to another.
+
+            </p>
+        </section>
+        </main>
+        <footer>
+            <p>  devloped by Rajesh. </p>
+        </footer>
+</body>
+</html>
